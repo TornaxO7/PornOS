06d52f39953b82d85d17e200c7145e6c3e6e2142
TornaxO7 <tornax07@gmail.com>
2022-02-22 22:37:30 +0100

    Trying to set up test environment

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
 #![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+use core::panic::PanicInfo;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+
+    for test in tests {
+        test();
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
 pub mod vga;
+
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,9 @@
 #![no_main]
 
 use pornos::println;
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    panic!("Pornos goes brrrrrrrrrrrr");
+    println!("I use pornos now");
     loop {}
 }
