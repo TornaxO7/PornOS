1100b7091129a0739a7dcf468a660552b419cadc
TornaxO7 <tornax07@gmail.com>
2022-02-23 03:09:35 +0100

    Trying to fix `cargo test`

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
 #![no_std]
+#![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(crate::test_starter)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-
-    for test in tests {
-        test();
-    }
-}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+pub fn test_starter(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
 
 pub mod vga;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(pornos::test_starter)]
+#![reexport_test_harness_main = "test_main"]
 
 use pornos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("I use pornos now");
+    println!("Startig up PornOS...");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
