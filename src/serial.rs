9cf93cd0fe2b8d15e92e135db55677950bc6148e
TornaxO7 <tornax07@gmail.com>
2022-03-18 18:10:39 +0100

    Adding isodir

---
+++ src/serial.rs
@@ -0,0 +1,33 @@
+use uart_16550::SerialPort;
+use spin::Mutex;
+use lazy_static::lazy_static;
+
+use core::fmt::Write;
+
+const SERIAL_PORT_ADDRESS: i32 = 0x3F8;
+
+lazy_static! {
+    pub static ref SERIAL1: Mute<SerialPort> = {
+        let mut serial_port = unsafe { SerialPort::new(SERIAL_PORT_ADDRESS) };
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    SERIAL1.lock().write_fmt(args).expect("Can't print on serial!");
+}
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+// #[macro_export]
+// macro_rules! serial_println {
+//     () => ($crate::serial_print!("\n"));
+//     ($fmt:expr) => ($crate::serial
+// }
