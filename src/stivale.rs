d4affbbdb687262b30a86d698f4df35789a52b4f
TornaxO7 <tornax07@gmail.com>
2022-04-01 15:06:02 +0200

    adding stack to linker script and added alignment

--- src/stivale.rs
+++ src/stivale.rs
@@ -1,13 +1,15 @@
 use stivale_boot::v2::{StivaleAnyVideoTag, StivaleHeader};
 
 pub const PORNOS_STACK_SIZE: usize = 8_192;
+
+#[link_section = ".porno_stack"]
 pub static PORNOS_STACK: [u8; PORNOS_STACK_SIZE] = [0; PORNOS_STACK_SIZE];
 
 #[used]
 #[no_mangle]
 #[link_section = ".stivale2hdr"]
 pub static STIVALE_HEADER: StivaleHeader = StivaleHeader::new()
-    .stack(unsafe { PORNOS_STACK.as_ptr().offset(PORNOS_STACK_SIZE as isize) })
+    .stack(PORNOS_STACK.as_ptr())
     .flags(0b11110)
     .tags(&ANY_VIDEO_HEADER_TAG as *const StivaleAnyVideoTag as *const ());
 
