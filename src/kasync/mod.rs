9535132e75dbddc1052085e323082505cb9159b5
TornaxO7 <tornax07@gmail.com>
2023-01-07 00:54:07 +0100

    managing dependencies
    - removing tinyvec
    + adding future

---
+++ src/kasync/mod.rs
@@ -0,0 +1,2 @@
+pub mod executer;
+pub mod spawner;
--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,27 @@
 
 extern crate alloc;
 
+pub mod kasync;
 pub mod gdt;
 pub mod interrupt;
 pub mod io;
 pub mod memory;
 
+async fn test_get_async() -> u32 {
+    69
+}
+
+async fn test_async() {
+    let number = test_get_async().await;
+    println!("Number: {}", number);
+}
+
 pub fn init() -> ! {
     gdt::init();
     interrupt::init();
     memory::paging::init_heap();
+
+    test_get_async();
 
     hlt_loop();
 }
@@ -28,6 +40,7 @@ pub fn tests() {
 }
 
 pub fn hlt_loop() -> ! {
+    println!("Entering halting loop...");
     loop {
         x86_64::instructions::hlt();
     }
