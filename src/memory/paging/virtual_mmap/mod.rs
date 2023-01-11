25db0c6820d348c380dfb4d6c1944547d3fdc528
TornaxO7 <tornax07@gmail.com>
2023-01-11 15:27:28 +0100

    removing TODO

--- src/memory/paging/virtual_mmap/mod.rs
+++ src/memory/paging/virtual_mmap/mod.rs
@@ -26,7 +26,6 @@ lazy_static! {
     pub static ref SIMP: Mutex<Mapper> = Mutex::new(Mapper::new());
 }
 
-// TODO: The functions from the general traits don't need a lock.
 // Create an extra layer for that with an attritbute with a mutex to lock the
 // actual unsafe mappings.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
